9b1a2da551b20971f43313c7ed74c06f215aa74d
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2025-03-11 13:02:33 -0400

    Setup database config, import database url

---
+++ config/pool.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+
+
+module.exports = new Pool({
+    connectionString: process.env.DB_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
