b07cac877e350e9726ebf94f4352863489ab21b7
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2025-03-12 13:13:00 -0400

    Setup passport config with LocalStrategy and setup user session

--- app.js
+++ app.js
@@ -16,4 +16,31 @@ app.use(express.urlencoded({ extended: true }));
 app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }));
 
 
+passport.use(new LocalStrategy(
+    (username, password, done) => {
+        pool.query("SELECT * FROM users WHERE email = $1", [username], (err, result) => {
+            if (err) return done(err);
+            const user = result.rows[0];
+            if (!user) return done(null, false, { message: 'No user with that email' });
+
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (err) return done(err);
+                if (res) return done(null, user);
+                else return done(null, false, { message: 'Incorrect password' });
+            });
+        })
+    }
+));
+
+
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser((id, done) => {
+    pool.query("SELECT * FROM users WHERE id=$1", [id], (e, result) => {
+        if (e) return done(e);
+        done(null, result.rows[0]);
+    });
+});
+
+app.use(passport.session());
+
 app.listen(8080, () => console.log('Server is running on 8080'))
