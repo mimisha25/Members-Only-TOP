e419a2b675ca692015d7f2694ddc3f34247c9dab
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2025-03-14 13:28:15 -0400

    Add svg profile photo for users, but the color of each user must different

--- config/db.js
+++ config/db.js
@@ -1,6 +1,5 @@
 const { Client } = require('pg');
 require('dotenv').config();
-
 const CREATE_SQL_USERS = `
 CREATE TABLE IF NOT EXISTS users (
    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -10,10 +9,17 @@ CREATE TABLE IF NOT EXISTS users (
     password VARCHAR(255) NOT NULL,
     membership_status BOOLEAN DEFAULT FALSE,
     admin BOOLEAN DEFAULT FALSE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+   profile_picture TEXT
 );
 `;
+
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b})`;
+}
 
 const CREATE_SQL_MESSAGES = `
 CREATE TABLE IF NOT EXISTS messages (
@@ -25,14 +31,19 @@ CREATE TABLE IF NOT EXISTS messages (
     status VARCHAR(50) DEFAULT 'active'
 );
 `;
+const svg = `<?xml version="1.0" encoding="UTF-8"?> 
+<svg xmlns="http://www.w3.org/2000/svg" width="50" height="80" ${getRandomColor()} class="bi bi-person-bounding-box img-fluid rounded-start" viewBox="0 0 16 16">
+    <path d="M1.5 1a.5.5 0 0 0-.5.5v3a.5.5 0 0 1-1 0v-3A1.5 1.5 0 0 1 1.5 0h3a.5.5 0 0 1 0 1zM11 .5a.5.5 0 0 1 .5-.5h3A1.5 1.5 0 0 1 16 1.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 1-.5-.5M.5 11a.5.5 0 0 1 .5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 1 0 1h-3A1.5 1.5 0 0 1 0 14.5v-3a.5.5 0 0 1 .5-.5m15 0a.5.5 0 0 1 .5.5v3a1.5 1.5 0 0 1-1.5 1.5h-3a.5.5 0 0 1 0-1h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 1 .5-.5" />
+    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+</svg>`;
 
 const date = new Date().toISOString();
 const INSERT_USERS = `
-INSERT INTO users (first_name, last_name, email, password, membership_status, admin, created_at)
+INSERT INTO users (first_name, last_name, email, password, membership_status, admin, created_at, profile_picture)
 VALUES 
-('Jane', 'Doe', 'janedoe@email.com', '123', true, true, '${date}'),
-('John', 'Doe', 'johndoe@email.com', '123', true, false, '${date}'),
-('James', 'Smith', 'jamessmith@email.com', '123', false, false, '${date}');
+('Jane', 'Doe', 'janedoe@email.com', '123', true, true, '${date}', '${svg}'),
+('John', 'Doe', 'johndoe@email.com', '123', true, false, '${date}', '${svg}'),
+('James', 'Smith', 'jamessmith@email.com', '123', false, false, '${date}', '${svg}');
 `;
 
 
--- config/queries.js
+++ config/queries.js
@@ -3,6 +3,7 @@ const { v4: uuid } = require('uuid');
 
 
 
+
 async function isAdmin() {
     try {
         const { rows } = await pool.query(
@@ -101,13 +102,26 @@ async function markInappropriate(id) {
         throw e;
     }
 }
-
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b})`;
+}
 async function signup(firstName, lastName, email, hashedPassword) {
     try {
+
+        const svgProfilePicture = `<?xml version="1.0" encoding="UTF-8"?> 
+        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="80" fill=${getRandomColor()} class="bi bi-person-bounding-box img-fluid rounded-start" viewBox="0 0 16 16">
+            <path d="M1.5 1a.5.5 0 0 0-.5.5v3a.5.5 0 0 1-1 0v-3A1.5 1.5 0 0 1 1.5 0h3a.5.5 0 0 1 0 1zM11 .5a.5.5 0 0 1 .5-.5h3A1.5 1.5 0 0 1 16 1.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 1-.5-.5M.5 11a.5.5 0 0 1 .5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 1 0 1h-3A1.5 1.5 0 0 1 0 14.5v-3a.5.5 0 0 1 .5-.5m15 0a.5.5 0 0 1 .5.5v3a1.5 1.5 0 0 1-1.5 1.5h-3a.5.5 0 0 1 0-1h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 1 .5-.5" />
+            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+        </svg>`;
         const { rows } = await pool.query(
-            'INSERT INTO users (first_name, last_name, email, password, membership_status) VALUES ($1, $2, $3, $4, $5) RETURNING *',
-            [firstName, lastName, email, hashedPassword, false]);
+            'INSERT INTO users (first_name, last_name, email, password, membership_status , profile_picture) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+            [firstName, lastName, email, hashedPassword, false, svgProfilePicture]);
+
         return rows[0];
+
     } catch (e) {
         console.error('Error signup user: ', e);
         throw e;
@@ -115,16 +129,14 @@ async function signup(firstName, lastName, email, hashedPassword) {
 }
 
 
-async function forum(req, res, isMember) {
+
+async function forum(isMember) {
     try {
         const query = isMember
-            ? 'SELECT messages.id, messages.title, messages.content, messages.timestamp, users.first_name, users.last_name, messages.user_id, messages.status FROM messages JOIN users ON messages.user_id=users.id'
+            ? 'SELECT messages.id, messages.title, messages.content, messages.timestamp, users.first_name, users.last_name,  users.profile_picture,messages.user_id, messages.status FROM messages JOIN users ON messages.user_id=users.id'
             : 'SELECT id, title, content, user_id, status  FROM messages';
-        pool.query(query, (e, result) => {
-            if (e) return res.send('Error fetching messages. Please try again later.');
-            console.log(result.rows);
-            res.render('index', { messages: result.rows || [], user: req.user });
-        });
+        const { rows } = await pool.query(query);
+        return rows;
     } catch (e) {
         console.error('Error forum: ', e);
         throw e;
