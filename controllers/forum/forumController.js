a11dfa82d26691f60f6367130cead290bbed48c6
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2025-03-14 13:29:15 -0400

    Render the index file to show all messages

--- controllers/forum/forumController.js
+++ controllers/forum/forumController.js
@@ -3,14 +3,16 @@ const db = require('../../config/queries');
 require('dotenv').config();
 
 
-
 async function forum(req, res) {
     try {
         const isMember = req.user && req.user.membership_status;
         const isLoggedIn = req.isAuthenticated();
+
         if (!isLoggedIn) return res.render('index', { messages: [], user: req.user });
-        const messages = await db.forum(req, res, isMember);
-        return messages;
+        const message = await db.forum(req, res, isMember);
+
+        return res.render('index', { messages: message, user: req.user });
+
     } catch (e) {
         console.error('Error in forum controller: ', e);
         res.status(500).send('Error in forum controller');
