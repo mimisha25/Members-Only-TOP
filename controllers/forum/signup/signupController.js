44a373371fd563d909da92cbec98fb033771b191
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2025-03-14 13:28:57 -0400

    refactor the code and add bscrypt package

--- controllers/forum/signup/signupController.js
+++ controllers/forum/signup/signupController.js
@@ -1,21 +1,21 @@
 const db = require('../../../config/queries');
 require('dotenv').config();
-async function signup(req, rea) {
+const bcrypt = require('bcryptjs')
 
-    async function signup(req, res) {
-        try {
-            const { firstName, lastName, email, password, confirmPassword } = req.body;
-            if (password !== confirmPassword) return res.send('Password do not match!');
-            const hashedPassword = await bcrypt.hash(password, 10);
-            const user = await db.signup(firstName, lastName, email, hashedPassword);
-            if (!user) return res.render('Error creating user');
-            res.redirect('/login');
+async function signup(req, res) {
+    try {
+        const { firstName, lastName, email, password, confirmPassword } = req.body;
+        if (password !== confirmPassword) return res.send('Password do not match!');
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await db.signup(firstName, lastName, email, hashedPassword);
+        if (!user) return res.render('Error creating user');
+        res.redirect('/login');
 
-        } catch (e) {
-            console.error('Error in creating user controller: ', e);
-            res.status(500).send('Error in creating user controller');
-        }
+    } catch (e) {
+        console.error('Error in creating user controller: ', e);
+        res.status(500).send('Error in creating user controller');
     }
+
 }
 
 module.exports = {
