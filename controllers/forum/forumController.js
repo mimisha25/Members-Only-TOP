638dc316eca714a98c00f20580d772a08ecdad22
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2025-03-16 16:21:10 -0400

    Show date as dd-mm-yyyy

--- controllers/forum/forumController.js
+++ controllers/forum/forumController.js
@@ -7,17 +7,20 @@ async function forum(req, res) {
     try {
         const isMember = req.user && req.user.membership_status;
         const isLoggedIn = req.isAuthenticated();
-
-        if (!isLoggedIn) return res.render('index', { messages: [], user: req.user });
+        if (!isLoggedIn) return res.render('forum', { messages: [], user: req.user });
         const message = await db.forum(req, res, isMember);
 
-        return res.render('index', { messages: message, user: req.user });
+        const date = new Date(new Date().toISOString());
+        const day = String(date.getDate()).padStart(2, '0');
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const year = date.getFullYear();
+        const formattedDate = `${day}-${month}-${year}`;
 
+        return res.render('forum', { messages: message, user: req.user, date: formattedDate });
     } catch (e) {
         console.error('Error in forum controller: ', e);
         res.status(500).send('Error in forum controller');
     }
-
 }
 
 
