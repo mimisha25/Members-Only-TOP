8f811a7fb922bf3460b9d4b31bb18e05715649d6
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2025-03-16 16:23:43 -0400

    If user is admin/member than show the right path, if not render the form

--- routes/forum/admin/isAdminRouter.js
+++ routes/forum/admin/isAdminRouter.js
@@ -2,8 +2,17 @@ const { Router } = require('express');
 const isAdminRouter = Router();
 require('dotenv').config();
 const controller = require('../../../controllers/forum/admin/isAdminController');
+const isAdmin = require('../../authMiddleware').isAdmin;
 
-isAdminRouter.get('/is-admin', (req, res) => res.render('isAdmin'));
+isAdminRouter.get('/is-admin', (req, res) => {
+    if (req.isAuthenticated() && req.user.admin) {
+        return res.render('partials/modal', {
+            text: 'You are an admin!'
+        });
+    } else {
+        return res.render('isAdmin');
+    }
+});
 isAdminRouter.post('/is-admin', controller.isAdmin);
 
 module.exports = isAdminRouter;
--- routes/forum/joinClub/joinclubRouter.js
+++ routes/forum/joinClub/joinclubRouter.js
@@ -2,10 +2,17 @@ const { Router } = require('express');
 const joinclubRouter = Router();
 require('dotenv').config();
 const controller = require('../../../controllers/forum/joinclub/joinClubController');
+const isAdmin = require('../../authMiddleware').isMember;
 
 joinclubRouter.get('/join-club', (req, res) => {
     if (!req.isAuthenticated()) return res.redirect('/login');
-    res.render('joinClub');
+    if (req.isAuthenticated() && req.user.membership_status) {
+        return res.render('partials/modal', {
+            text: 'You are a member!'
+        });
+    } else {
+        return res.render('joinClub');
+    }
 })
 joinclubRouter.post('/join-club', controller.member);
 module.exports = joinclubRouter;
