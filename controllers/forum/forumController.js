f103c134573b8310940805065601b63c6188c030
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2025-04-08 17:02:02 -0400

    Implement controller to forum route

--- controllers/forum/forumController.js
+++ controllers/forum/forumController.js
@@ -4,23 +4,16 @@ require('dotenv').config();
 
 
 async function forum(req, res) {
-    try {
-        const isMember = req.user && req.user.membership_status;
-        const isLoggedIn = req.isAuthenticated();
-        if (!isLoggedIn) return res.render('forum', { messages: [], user: req.user });
-        const message = await db.forum(req, res, isMember);
-
-        const date = new Date(new Date().toISOString());
-        const day = String(date.getDate()).padStart(2, '0');
-        const month = String(date.getMonth() + 1).padStart(2, '0');
-        const year = date.getFullYear();
-        const formattedDate = `${day}-${month}-${year}`;
-
-        return res.render('forum', { messages: message, user: req.user, date: formattedDate });
-    } catch (e) {
-        console.error('Error in forum controller: ', e);
-        res.status(500).send('Error in forum controller');
-    }
+    const isMember = req.user && req.user.membership_status;
+    const isLoggedIn = req.isAuthenticated();
+    if (!isLoggedIn) return res.render('forum', { messages: [], user: req.user });
+    const message = await db.forum(req, res, isMember);
+    const date = new Date(new Date().toISOString());
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    const formattedDate = `${day}-${month}-${year}`;
+    return res.render('forum', { messages: message, user: req.user, date: formattedDate });
 }
 
 
--- routes/forum/forumRouter.js
+++ routes/forum/forumRouter.js
@@ -1,7 +1,8 @@
 const { Router } = require('express');
 const forumRouter = Router();
 const controller = require('../../controllers/forum/forumController');
+const catchAsync = require('../../utils/catchAsync');
 
-forumRouter.get('/forum', controller.forum);
+forumRouter.get('/forum', catchAsync(controller.forum));
 
 module.exports = forumRouter;
