60d6a4d93e8e4904e11df76cab07fc0507f1060f
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2025-04-08 17:03:42 -0400

    Implement controller on forum router

--- controllers/forum/message/messageController.js
+++ controllers/forum/message/messageController.js
@@ -1,115 +1,69 @@
 const db = require('../../../config/queries');
 require('dotenv').config();
-
+const ExpressError = require('../../../utils/ExpressError')
 
 async function createMessage(req, res) {
-    try {
-        const { title, content } = req.body;
-        console.log('Title:', title);
-        console.log('Content:', content);
-        console.log('User ID:', req.user.id);
-        const newMessage = await db.createMessage(title, content, req.user.id);
-        if (!newMessage) return res.status(400).send('Error creating message');
-        console.log('New Message:', newMessage);
-
-        res.redirect('/forum');
-    } catch (e) {
-        console.error('Error in creating messages controller: ', e);
-        res.status(500).send('Error in creating messages controller');
-    }
+    const { title, content } = req.body;
+    const newMessage = await db.createMessage(title, content, req.user.id);
+    if (!newMessage) throw new ExpressError('Error in creating message', 400);
+    res.redirect('/forum');
 }
 
 
 async function deleteMessage(req, res) {
-    try {
-        const isMember = req.user && req.user.membership_status;
-        const { id } = req.params;
-        const userId = req.user.id;
-        const isAdmin = req.user.admin;
-        console.log(`Attempting to delete message with ID: ${id} by user: ${userId}`);
-        const message = await db.deleteMessage(id, isAdmin, userId);
-        if (!message) return res.send('Error deleting message');
-
-
-        res.redirect('/forum');
-    } catch (e) {
-        console.error('Error in deleting messages controller: ', e);
-        res.status(500).send('Error in deleting messages controller');
-    }
+    const isMember = req.user && req.user.membership_status;
+    const { id } = req.params;
+    const userId = req.user.id;
+    const isAdmin = req.user.admin;
+    const message = await db.deleteMessage(id, isAdmin, userId);
+    if (!message) throw new ExpressError('Error in deleting message', 400);
+    res.redirect('/forum');
 }
 
 
 async function editMessage(req, res) {
-    try {
-        const messageId = req.params.id;
-        const userId = req.user.id;
-        console.log('Attempting to fetch message with ID:', messageId, 'for user ID:', userId);
-
-        const message = await db.editMessage(userId, messageId);
-        if (!message) return res.send('Message not found or you are not authorized to edit this message.');
-        res.render('editMessage', { message: message });
-    } catch (e) {
-        console.error('Error in editing messages controller: ', e);
-        res.status(500).send('Error in editing messages controller');
-    }
+    const messageId = req.params.id;
+    const userId = req.user.id;
+    const message = await db.editMessage(userId, messageId);
+    if (!message) throw new ExpressError('Message not found or you are not authorized to edit this message.', 400);
+    res.render('editMessage', { message: message });
 };
 
 async function postEditMessage(req, res) {
-    try {
-        const messageId = req.params.id;
-        const userId = req.user.id;
-        const { title, content } = req.body;
-        const message = await db.postEditMessage(title, content, messageId, userId);
-        if (!message) {
-            console.error('Error updating message:', e);
-            return res.send('Error updating message.');
-        }
-        if (message.length === 0) return res.send('Message not found or you are not authorized to edit this message.');
-        console.log('Message updated successfully:', message);
-        res.redirect('/forum');
-    } catch (e) {
-        console.error('Error in post editing messages controller: ', e);
-        res.status(500).send('Error in post editing messages controller');
-    }
+    const messageId = req.params.id;
+    const userId = req.user.id;
+    const { title, content } = req.body;
+    const message = await db.postEditMessage(title, content, messageId, userId);
+    if (!message) throw new ExpressError('Error in updating message .', 400);
+    if (message.length === 0) throw new ExpressError('Any message got to data.', 400);
+    res.redirect('/forum');
 };
 
 
 async function markInappropriate(req, res) {
-    try {
-        const { id } = req.params;
-        const message = await db.markInappropriate(id);
-        if (!message) {
-            console.error('Error marking message as inappropriate:', e);
-            return res.send('Error marking message.');
-        }
-        if (message.length === 0) return res.send('Message not found.')
-        console.log('Message marked as inappropriate:', message);
-        res.redirect('/forum');
-    } catch (e) {
-        console.error('Error in post editing messages controller: ', e);
-        res.status(500).send('Error in post editing messages controller');
-    }
+    const { id } = req.params;
+    const message = await db.markInappropriate(id);
+    if (!message) throw new ExpressError('Error marking message as inappropriate', 400);
+    if (message.length === 0) throw new ExpressError('Message not found', 400);
+    res.redirect('/forum');
+
 }
 
-async function forum(isMember) {
-    try {
-        const query = isMember
-            ? 'SELECT messages.id, messages.title, messages.content, messages.timestamp, users.first_name, users.last_name, messages.user_id, messages.status FROM messages JOIN users ON messages.user_id=users.id'
-            : 'SELECT id, title, content, user_id, status  FROM messages';
-        pool.query(query, (e, result) => {
-            if (e) return res.send('Error fetching messages. Please try again later.');
-            res.render('index', { messages: result.rows || [], user: req.user });
-        })
-    } catch (e) {
-        console.error('Error in post editing messages controller: ', e);
-        res.status(500).send('Error in post editing messages controller');
-    }
-}
+// async function forum(isMember) {
+//     const query = isMember
+//         ? 'SELECT messages.id, messages.title, messages.content, messages.timestamp, users.first_name, users.last_name, messages.user_id, messages.status FROM messages JOIN users ON messages.user_id=users.id'
+//         : 'SELECT id, title, content, user_id, status  FROM messages';
+//     pool.query(query, (e, result) => {
+//         if (e) return res.send('Error fetching messages. Please try again later.');
+//         res.render('index', { messages: result.rows || [], user: req.user });
+//     })
+
+// }
 module.exports = {
     createMessage,
     deleteMessage,
     editMessage,
     postEditMessage,
     markInappropriate,
-    forum
+    // forum
 }
--- routes/forum/message/messageRouter.js
+++ routes/forum/message/messageRouter.js
@@ -2,27 +2,28 @@ const { Router } = require('express');
 const messageRouter = Router();
 require('dotenv').config();
 const controller = require('../../../controllers/forum/message/messageController');
+const catchAsync = require('../../../utils/catchAsync');
 
 messageRouter.get('/message/new', (req, res) => {
     if (!req.isAuthenticated()) return res.redirect('/login');
     res.render('newMessage');
 });
 
-messageRouter.post('/message/new', (req, res) => {
+messageRouter.post('/message/new', catchAsync(async (req, res) => {
     if (!req.isAuthenticated()) return res.redirect('/login');
-    controller.createMessage(req, res)
-});
+    await controller.createMessage(req, res)
+}));
 
-messageRouter.post('/message/delete/:id', (req, res) => {
+messageRouter.post('/message/delete/:id', catchAsync(async (req, res) => {
     if (!req.isAuthenticated()) return res.redirect('/login');
-    controller.deleteMessage(req, res);
-})
+    await controller.deleteMessage(req, res);
+}))
 
-messageRouter.get('/message/edit/:id', controller.editMessage);
-messageRouter.post('/message/edit/:id', controller.postEditMessage);
-messageRouter.post('/message/mark-inappropriate/:id', (req, res) => {
+messageRouter.get('/message/edit/:id', catchAsync(controller.editMessage));
+messageRouter.post('/message/edit/:id', catchAsync(controller.postEditMessage));
+messageRouter.post('/message/mark-inappropriate/:id', catchAsync(async (req, res) => {
     if (!req.isAuthenticated() || !req.user.admin) return res.send('You are not authorized to perform this action');
-    controller.markInappropriate(req, res);
-})
+    await controller.markInappropriate(req, res);
+}))
 
 module.exports = messageRouter;
